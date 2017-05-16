658703d2bb1a58db93bca64bc21509b32f302b0b
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-05-15 22:25:23 -0500

    video browser app, with search feature completed

--- video-app/src/index.js
+++ video-app/src/index.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import configData from '../../../config.json'; // Get API KEYS from local config file
 var config = configData || "";
-
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = config.YT_API; 
 
-const App = () => {
-  return( 
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('cats');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState ({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });    
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
