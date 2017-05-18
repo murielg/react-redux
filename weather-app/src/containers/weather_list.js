8e8de354c72d22438d7d1a651da6406dea7a24c1
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-05-18 10:56:04 -0500

    integrated google maps, style fixes:

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,21 +1,25 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather (cityData) {
     const name = cityData.city.name;
-    const strokeWidth = 0.3;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const strokeWidth = 0.25;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => _.round(_.subtract(_.multiply(temp,9/5), 459.67)));
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
     
+
     return (
       <tr key={name} >
-        <td>{ name }</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} type="temperature" colors={{strokeWidth:strokeWidth, stroke: "rgba(203, 75, 22,1)", fill: "rgba(203, 75, 22,0.5)" }} /></td>
         <td><Chart data={pressures} type="pressure" colors={{strokeWidth:strokeWidth, stroke: "rgba(38, 139, 210,1)", fill: "rgba(38, 139, 210,0.5)" }} /></td>
-        <td><Chart data={humidities} type="humidity" colors={{strokeWidth:strokeWidth, stroke: "rgba(42, 161, 152,0.1)", fill: "rgba(42, 161, 152,0.5)" }} /></td>
+        <td><Chart data={humidities} type="humidity" colors={{strokeWidth:strokeWidth, stroke: "rgba(42, 161, 152,1)", fill: "rgba(42, 161, 152,0.5)" }} /></td>
       </tr>
     );
   }
