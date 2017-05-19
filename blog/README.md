10102649bd4c15916088a397f77de6db7218a4cf
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-05-19 17:18:29 -0500

    express test. partially working.

--- blog/README.md
+++ blog/README.md
@@ -2,6 +2,8 @@
 
 CRUD App using forms with validation, react router, etc. 
 
+Use with your own API or clone down a copy of my very basic Rails 5 [REST API](https://github.com/murielg/rest-api)
+
 ### You know what to do
 
 - Install dependencies with ` yarn install `
@@ -9,3 +11,5 @@ CRUD App using forms with validation, react router, etc.
 - Watch and develop with ` npm start `
 
 - Build with `npm run build`
+
+
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,17 +1,31 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends Component {
+  render() { return <div>Hello</div>}
+}
+class Goodbye extends Component {
+  render() { return <div>Goodbye</div>}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/hello" component={Hello} />
+      </div>
+    </BrowserRouter> 
   </Provider>
   , document.querySelector('.container'));
+
