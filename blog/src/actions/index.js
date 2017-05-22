0e743d0548ed723ee6801bab189109a0be4f6716
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-05-22 17:44:56 -0500

    adding axios and redux promise, setting up action creator to fetch posts. unfinished

---
+++ blog/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+export function fetchPosts() {
+  //const request = axious.get('/');
+
+  return {
+    type: FETCH_POSTS
+  }
+}
+
--- blog/src/index.js
+++ blog/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
+
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
 import './scss/style.scss';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
