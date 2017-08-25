3089e55a330af94ddabf4bbf3ad07833f791c75b
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-08-25 11:56:23 -0500

    refux form

--- redux-form/src/components/app.js
+++ redux-form/src/components/app.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-
-import Button from './button';
+import React from 'react';
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
-        <Button title="Tap Me" />
+        {this.props.children}
       </div>
     );
   }
