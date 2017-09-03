a187ce346f356883caad4682301adfdb053f66c7
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-02 22:08:08 -0500

    search bar

--- search-filter-map/src/components/app.js
+++ search-filter-map/src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import LocationsList from "../containers/locations_list";
+import SearchBar from "../containers/search_bar";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="row">
+        <SearchBar/>
         <LocationsList/>
       </div>
     );
