b2c233117477305ce4ffbde490362f57c337e64f
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-03 03:08:45 -0500

    filters setup

--- search-filter-map/src/components/app.js
+++ search-filter-map/src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import LocationsList from "../containers/locations_list";
-import SearchBar from "../containers/search_bar";
+import SearchBar from "./search_bar";
 import LocationsMap from "../containers/locations_map";
+import FiltersContainer from "../containers/filters_container";
 
 export default class App extends Component {
   render() {
     return (
         <div className="container-fluid">
             <div className="row">
-                <SearchBar/>
+                <FiltersContainer/>
                 <LocationsList/>
                 <LocationsMap/>
             </div>
