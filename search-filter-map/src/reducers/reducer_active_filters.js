26ec31c275bcaf590df746416c00f66ddfbdb913
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-03 15:26:43 -0500

    filters

--- search-filter-map/src/reducers/reducer_active_filters.js
+++ search-filter-map/src/reducers/reducer_active_filters.js
@@ -1,4 +1,5 @@
 import {SET_FILTERS} from "../actions/types";
+import _ from 'lodash';
 //state is not application state, just the state
 //this reducer is responsible for, e.g. creating books
 //if state is undefined, set to null
@@ -6,10 +7,11 @@ import {SET_FILTERS} from "../actions/types";
 export default function(state = {}, action) {
     switch (action.type) {
         case SET_FILTERS:
-            console.log(SET_FILTERS);
-            return action.payload;
+
+            let newState = _.union([...state], [action.payload]);
+
+            return newState;
     }
 
     return state;
 }
-
