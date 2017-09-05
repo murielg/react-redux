143a88a40baf7c131b8cbe2c8b61a3d343ca68a0
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-05 17:56:21 -0500

    app

--- search-filter-map/src/components/app.js
+++ search-filter-map/src/components/app.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import LocationsList from "../containers/locations_container";
 import LocationsMap from "../containers/locations_map";
 import FiltersContainer from "../containers/filters_container";
+import GeolocationContainer from "../containers/geolocation_container";
 
 export default class App extends Component {
   render() {
     return (
         <div className="container-fluid">
             <div className="row">
+              <div className="col-sm-3">
+                <GeolocationContainer/>
                 <FiltersContainer/>
+              </div>
                 <LocationsList/>
                 <LocationsMap/>
             </div>
