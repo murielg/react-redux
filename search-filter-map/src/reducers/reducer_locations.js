9fa6a808e072d0a38fdcdcecdefc2a064ab90280
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-05 16:26:51 -0500

    locations

--- search-filter-map/src/reducers/reducer_locations.js
+++ search-filter-map/src/reducers/reducer_locations.js
@@ -1,49 +1,49 @@
-export default function() {
-    return [
-        {
-            "id" : "DFW",
-            "name" : "DFW International Airport",
-            "position" : {"lat":32.9222,"lng":-97.0409},
-            "elevation" : "603",
-            "runway-length" : "11388",
-            "city" : "Dallas",
-            "size" : "large"
-        },
-        {
-            "id" : "DAL",
-            "name" : "Dallas Love Field Airport",
-            "position" : {"lat":32.8439,"lng":-96.8496},
-            "elevation" : "487",
-            "runway-length" : "8800",
-            "city" : "Dallas",
-            "size" : "medium"
-        },
-        {
-            "id" : "AUS",
-            "name" : "Austin-Bergstrom International Airport",
-            "position" : {"lat":30.2027,"lng":-97.6653},
-            "elevation" : "542",
-            "runway-length" : "12248",
-            "city" : "Austin",
-            "size" : "medium"
-        },
-        {
-            "id" : "IAH",
-            "name" : "George Bush Intercontinental Airport",
-            "position" : {"lat":29.9784,"lng":-95.3424},
-            "elevation" : "98",
-            "runway-length" : "12001",
-            "city" : "Houston",
-            "size" : "medium"
-        },
-        {
-            "id" : "ADS",
-            "name" : "Addison Airport",
-            "position" : {"lat":32.968611,"lng":-96.836389},
-            "elevation" : "644",
-            "runway-length" : "7202",
-            "city" : "Dallas",
-            "size" : "small"
-        }
-    ];
+export default function () {
+  return [
+    {
+      "id": "DFW",
+      "name": "DFW International Airport",
+      "position": {"lat": 32.9222, "lng": -97.0409},
+      "elevation": "603",
+      "runway-length": "11388",
+      "city": "Dallas",
+      "attributes": ["dallas", "large"]
+    },
+    {
+      "id": "DAL",
+      "name": "Dallas Love Field Airport",
+      "position": {"lat": 32.8439, "lng": -96.8496},
+      "elevation": "487",
+      "runway-length": "8800",
+      "city": "Dallas",
+      "attributes": ["dallas", "medium"]
+    },
+    {
+      "id": "AUS",
+      "name": "Austin-Bergstrom International Airport",
+      "position": {"lat": 30.2027, "lng": -97.6653},
+      "elevation": "542",
+      "runway-length": "12248",
+      "city": "Austin",
+      "attributes": ["austin", "large"]
+    },
+    {
+      "id": "IAH",
+      "name": "George Bush Intercontinental Airport",
+      "position": {"lat": 29.9784, "lng": -95.3424},
+      "elevation": "98",
+      "runway-length": "12001",
+      "city": "Houston",
+      "attributes": ["houston", "large"]
+    },
+    {
+      "id": "ADS",
+      "name": "Addison Airport",
+      "position": {"lat": 32.968611, "lng": -96.836389},
+      "elevation": "644",
+      "runway-length": "7202",
+      "city": "Dallas",
+      "attributes": ["dallas", "small"]
+    }
+  ];
 }
