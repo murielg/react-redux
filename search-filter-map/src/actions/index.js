875e3311491c4facae100dc8b31c7ecf8e5b676a
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-05 13:35:10 -0500

    getGeolocation action

--- search-filter-map/src/actions/index.js
+++ search-filter-map/src/actions/index.js
@@ -1,29 +1,37 @@
 import {
-    LOCATION_SELECTED,
-    REMOVE_FILTER,
-    ADD_FILTER
+  LOCATION_SELECTED,
+  REMOVE_FILTER,
+  ADD_FILTER,
+  GET_GEOLOCATION
 } from "./types";
 
 export function selectLocation(location) {
-    // selectLocation is an ActionCreator
-    // will return an action, ie an object with a type property
-    return {
-        type: LOCATION_SELECTED,
-        payload: location
-    }
+  // selectLocation is an ActionCreator
+  // will return an action, ie an object with a type property
+  return {
+    type: LOCATION_SELECTED,
+    payload: location
+  }
 }
 
 export function addFilter(filter) {
-    return {
-        type: ADD_FILTER,
-        payload: filter
-    }
+  return {
+    type: ADD_FILTER,
+    payload: filter
+  }
 
 }
 
 export function removeFilter(filter) {
-    return {
-        type: REMOVE_FILTER,
-        payload: filter
-    }
+  return {
+    type: REMOVE_FILTER,
+    payload: filter
+  }
+}
+
+export function getGeolocation(location) {
+  return {
+    type: GET_GEOLOCATION,
+    payload: location
+  }
 }
