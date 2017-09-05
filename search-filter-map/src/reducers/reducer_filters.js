836ba1ed8f51ec5eef6cd71048d2e77e68274245
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-09-05 18:19:00 -0500

    refactoring filters reducer

--- search-filter-map/src/reducers/reducer_filters.js
+++ search-filter-map/src/reducers/reducer_filters.js
@@ -1,23 +1,24 @@
-import _ from 'lodash';
-
 import {
-    REMOVE_FILTER,
-    ADD_FILTER
+  REMOVE_FILTER,
+  ADD_FILTER
 } from "../actions/types";
 
 //In this context, state is not application state,
 // just the state this reducer is responsible for.
 //if state is undefined, set to empty []
-export default function(state = [] , action) {
-    switch (action.type) {
-        case ADD_FILTER:
-            return _.union([action.payload], [...state]);
+export default function (state = [], action) {
+  switch (action.type) {
+    case ADD_FILTER:
+      if ([...state].indexOf(action.payload) === -1) {
+        return [...state, action.payload];
+      }
+      return state;
 
-        case REMOVE_FILTER:
-            return state.filter(item => item !== action.payload);
+    case REMOVE_FILTER:
+      return state.filter(item => item !== action.payload);
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 
 }
